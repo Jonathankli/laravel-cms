36e002c886e10659f01cd5645c33e3c8025f8597
Jonahan Klinger <jo@cfmmedia.de>
2022-11-14 17:28:01 +0100

    global provider

---
+++ resources/js/contexts/GlobalProvider.tsx
@@ -0,0 +1,38 @@
+import {
+    ColorScheme,
+    ColorSchemeProvider,
+    MantineProvider,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import { ModalsProvider } from "@mantine/modals";
+import React from "react";
+
+const GlobalProviders = (props) => {
+    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+        key: "mantine-color-scheme",
+        defaultValue: "dark",
+        getInitialValueInEffect: true,
+    });
+
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+    return (
+        <ColorSchemeProvider
+            colorScheme={colorScheme}
+            toggleColorScheme={toggleColorScheme}
+        >
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    colorScheme,
+                }}
+            >
+                <ModalsProvider>{props.children}</ModalsProvider>
+            </MantineProvider>
+        </ColorSchemeProvider>
+    );
+};
+
+export default GlobalProviders;

0e2b1dc817c41b836de6b72b897135d27aba1419
Jonahan Klinger <jo@cfmmedia.de>
2023-07-18 21:18:03 +0700

    refactor shell layout

--- resources/js/layouts/ShellLayout.tsx
+++ resources/js/layouts/ShellLayout.tsx
@@ -1,25 +1,16 @@
-import { AppShell } from '@mantine/core';
-import { ModalsProvider } from '@mantine/modals';
-import * as React from 'react';
-import { ShellHeader } from '../components/ShellHeader/ShellHeader';
-import { GlobalObjectEditor, GlobalObjectPicker } from '../features/object';
+import React from "react";
+import { ShellHeader } from "../components/ShellHeader/ShellHeader";
+import { GlobalObjectEditor, GlobalObjectPicker } from "../features/object";
 
 const ShellLayout = (props) => {
     return (
-        <ModalsProvider>
-            <AppShell
-                padding="md"
-                header={<ShellHeader />}
-                styles={(theme) => ({
-                    main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-                })}
-                >
-                <GlobalObjectPicker />
-                <GlobalObjectEditor />
-                {props.children}
-            </AppShell>
-        </ModalsProvider>
-     );
-}
- 
+        <>
+            <ShellHeader />
+            <GlobalObjectPicker />
+            <GlobalObjectEditor />
+            {props.children}
+        </>
+    );
+};
+
 export default ShellLayout;
